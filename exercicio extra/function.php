aa3c67ce598385d0377cc6d5fc52bf3c3b6dddbe
Julio Henrique <tecjuliohenrique@gmail.com>
2016-09-24 10:16:04 -0300

    aula3

--- exercicio extra/function.php
+++ exercicio extra/function.php
@@ -17,3 +17,20 @@ function prime(){
 
 $entrada_do = readline();
 $a = prime(entrada_do);
+
+
+//calcular tempo de execucao 
+<?php
+// Iniciamos o "contador"
+list($usec, $sec) = explode(' ', microtime());
+$script_start = (float) $sec + (float) $usec;
+ 
+/* SEU CÓDIGO PHP */
+ 
+// Terminamos o "contador" e exibimos
+list($usec, $sec) = explode(' ', microtime());
+$script_end = (float) $sec + (float) $usec;
+$elapsed_time = round($script_end - $script_start, 5);
+
+// Exibimos uma mensagem
+echo 'Elapsed time: ', $elapsed_time, ' secs. Memory usage: ', round(((memory_get_peak_usage(true) / 1024) / 1024), 2), 'Mb';
